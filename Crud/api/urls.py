3461ba9483ed2cad1c0c14cb03817f5950466af8
shishirsalunkhebrightcrestsolutions <shishir.salunkhe@brightcrestsolutions.com>
2025-12-18 13:39:03 +0530

    Delete Crud directory

--- Crud/api/urls.py
+++
@@ -1,9 +0,0 @@
-
-from epense import views
-from django.urls import path, include
-
-urlpatterns = [
-     path('get-transactions/', views.get_transactions),
-     path('transactions/',views.TransactionAPI.as_view())
-    
-]
